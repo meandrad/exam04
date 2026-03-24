aabd37ffb72b34b7985331dd306a626180e39250
Melissa <memejaragua@gmail.com>
2026-03-24 14:44:26 -0300

    vbc

---
+++ lvl2/vbc/vbc.h
@@ -0,0 +1,15 @@
+#ifndef VBC_H
+# define VBC_H
+
+#include <ctype.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+void unexpected(char c);
+int ft_sum();
+int ft_product();
+int ft_factor();
+int check_input(char *str);
+
+#endif
